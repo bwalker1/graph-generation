6a158ca1de176847f2e9abb1a221ef099116cfef
bwalker1 <bwalker@unc.edu>
2018-11-08 16:37:30 -0500

    unprinted

--- train.py
+++ train.py
@@ -876,7 +876,7 @@ def train_rnn_encoder_epoch(epoch, args, rnn, data_loader,
         
         # use cross entropy loss
         Z = data['Z'].long().to(device)
-        print(Z_pred)
+        #print(Z_pred)
         #print(Z)
         loss = nn.CrossEntropyLoss()(Z_pred,torch.max(Z,1)[1])
         loss.backward()
