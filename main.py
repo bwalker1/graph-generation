4fedf5378c8d7c9bac59866363ce638bde8bdef6
wweir827 <wweir827@gmail.com>
2018-11-30 08:45:38 -0500

    modified main to generate graphs from all conditions

--- main.py
+++ main.py
@@ -223,15 +223,22 @@ if __name__ == '__main__':
             fname = args.model_save_path + fns.fname + 'output_' + str(args.load_epoch) + '_cond=' + str(args.conditional) + '.dat'
             output.load_state_dict(torch.load(fname,map_location='cpu'))
 
-
-        # how many to generate
         list_length = 1000
         # desired Z value (if you're using conditonal
-        Z = torch.Tensor([[1, 0]]*list_length) if args.conditional else None
-        # Generate a graph list
-        G = graph_gen(args, rnn,output,Z,args.max_prev_node,args.max_num_node,list_length)
-        # save the graphs
-        save_graph_list(G, fns.fname_test2)
+        if args.conditional:
+            for i, Z in enumerate([[1, 0], [0, 1]]):
+                Z = torch.Tensor(Z)
+                # Generate a graph list
+                G = graph_gen(args, rnn, output, Z, args.max_prev_node, args.max_num_node, list_length)
+                # save the graphs
+
+                save_graph_list(G, fns.fname_test2 + "_{:d}".format(i))
+        else:
+            Z = None
+            # Generate a graph list
+            G = graph_gen(args, rnn, output, Z, args.max_prev_node, args.max_num_node, list_length)
+            # save the graphs
+            save_graph_list(G, fns.fname_test2)
 
     ### graph completion
     # train_graph_completion(args,dataset_loader,rnn,output)
